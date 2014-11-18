d83e0ce00e44a86709ee2cb669b5b682908b9881
Jesse Panganiban <me@jpanganiban.com>
2014-11-18 17:28:09 +0800

    Awesome

--- captcha/imkeys.php
+++ captcha/imkeys.php
@@ -1,4 +1,4 @@
 <?php
 	$oNameList = array("pes","nxx","6p3","v8x","2hu","jlj","frx","txr","dek","pc8");
-	$oCharList = array("P","U","7","G","E","2","C","T","R","X");
+	$oCharList = array("6","Z","E","E","P","X","J","K","2","U");
 // End of file imkeys.php
